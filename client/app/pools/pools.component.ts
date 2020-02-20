3d17e39951bd0c6ce7f660f95f740fccdf4f47c2
ljulianal <ljulianal@ibm.com>
2020-02-20 16:05:44 -0500

    Colores grafica

--- client/app/pools/pools.component.ts
+++ client/app/pools/pools.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
 import * as pluginDataLabels from 'chartjs-plugin-datalabels';
-import { Label } from 'ng2-charts';
+import { Label, Colors } from 'ng2-charts';
 import {
   FormBuilder,
   FormControl,
@@ -23,7 +23,8 @@ export class PoolsComponent implements OnInit {
   public barChartOptions: ChartOptions = {
     responsive: true,
     // We use these empty structures as placeholders for dynamic theming.
-    scales: { xAxes: [{}], yAxes: [{}] },
+    scales: { xAxes: [{}], 
+    yAxes: [{}] },
     plugins: {
       datalabels: {
         anchor: 'end',
@@ -31,14 +32,16 @@ export class PoolsComponent implements OnInit {
       }
     }
   };
-  public barChartLabels: Label[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  public barChartLabels: Label[] = ['2006'];
   public barChartType: ChartType = 'bar';
+  public barChartColors: Array<any>=[{ backgroundColor:'rgb(72,202,105)'}, {backgroundColor:'rgb(245,48,29)'}]
   public barChartLegend = true;
   public barChartPlugins = [pluginDataLabels];
 
   public barChartData: ChartDataSets[] = [
-    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' }
+    
+    { data: [65], label: 'Series A' },
+    { data: [28], label: 'Series B' }
   ];
 
 
@@ -50,7 +53,6 @@ export class PoolsComponent implements OnInit {
   public chartHovered({ event, active }: { event: MouseEvent, active: {}[] }): void {
     console.log(event, active);
   }
-
   public randomize(): void {
     // Only Change 3 values
     const data = [
